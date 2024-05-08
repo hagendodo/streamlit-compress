c40f20a31310f4768b0aa89351519003847cdc8c
ibnuwdodo <ibnuwdodo@gmail.com>
2024-05-08 15:49:52 +0700

    fix

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import streamlit as st
 import os
 from pydub import AudioSegment
+from io import BytesIO
 
 # Function to compress audio
 def compress_audio(input_file, bitrate='64k'):
     audio = AudioSegment.from_file(input_file)
     compressed_audio = audio.set_frame_rate(44100).set_channels(1)
-    return compressed_audio.export(format='mp3', bitrate=bitrate)
+    output_buffer = BytesIO()
+    compressed_audio.export(output_buffer, format='mp3', bitrate=bitrate)
+    return output_buffer.getvalue()
 
 # Main function
 def main():
-    st.title("Audio Compression App")
+    st.title("Audio Compression Apps")
     
     # Sidebar
     st.sidebar.title("Settings")
@@ -30,8 +33,6 @@ def main():
         file_details = {"Filename":audio_file.name,"FileType":audio_file.type,"FileSize":audio_file.size}
         st.write(file_details)
         
-        print("Audio file:", audio_file)  # Print audio file for debugging
-        
         # Compress button
         if st.button("Compress"):
             st.write("Compressing...")
@@ -42,7 +43,6 @@ def main():
             st.write("### Download Compressed Audio")
             download_button_str = f"Download Compressed Audio File ({os.path.splitext(audio_file.name)[0]}_compressed.mp3)"
             st.download_button(label=download_button_str, data=compressed_audio, file_name=f"{os.path.splitext(audio_file.name)[0]}_compressed.mp3", mime="audio/mpeg", key=None)
-
 
 # Run the app
 if __name__ == '__main__':
